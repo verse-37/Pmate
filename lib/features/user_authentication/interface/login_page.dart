537ae96143deeba0401145295bcc2a33e0459312
verse-37 <verse37theone@gmail.com>
2024-11-23 07:28:01 +0700

    v0.1.5. Log in and sign up page initialized

--- lib/features/user_authentication/interface/login_page.dart
+++ lib/features/user_authentication/interface/login_page.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final local = AppLocalizations.of(context);
+
+    void onExit() {
+      Navigator.pop(context);
+    }
+
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              IconButton(
+                onPressed: onExit,
+                icon: Icon(Icons.turn_left_rounded),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
