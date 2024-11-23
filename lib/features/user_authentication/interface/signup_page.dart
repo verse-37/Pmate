6f519c4c658eece65ecfbe806f5602045285a5f0
verse-37 <verse37theone@gmail.com>
2024-11-23 17:42:10 +0700

    v0.1.12. Sign up UI completed, auth_service created in the business section

--- lib/features/user_authentication/interface/signup_page.dart
+++ lib/features/user_authentication/interface/signup_page.dart
@@ -1,31 +1,243 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+// ignore: unused_import
+import 'package:logger/logger.dart';
+import 'package:pmate/env/common/buttons.dart';
+import 'package:pmate/features/user_authentication/interface/login_page.dart';
 
 class SignupPage extends StatelessWidget {
   const SignupPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final local = AppLocalizations.of(context);
-
-    void onExit() {
-      Navigator.pop(context);
-    }
-
     return Scaffold(
+      resizeToAvoidBottomInset: true,
+      bottomNavigationBar: LoginLink(),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              IconButton(
-                onPressed: onExit,
-                icon: Icon(Icons.turn_left_rounded),
-              ),
-            ],
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: 10.0,
+              horizontal: 25.0,
+            ),
+            child: SignupForm(),
           ),
         ),
       ),
+    );
+  }
+}
+
+class LoginLink extends StatelessWidget {
+  const LoginLink({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final local = AppLocalizations.of(context);
+    final theme = Theme.of(context);
+
+    void onSignup() {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => LoginPage(),
+        ),
+      );
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(
+          children: [
+            TextSpan(
+                text: local.sign_up_account_already,
+                style: TextStyle(
+                  color: theme.colorScheme.secondary,
+                  fontWeight: FontWeight.normal,
+                  fontSize: theme.textTheme.bodyMedium?.fontSize ?? 16,
+                )),
+            TextSpan(
+              text: local.log_in,
+              style: theme.textTheme.bodyMedium?.copyWith(
+                color: theme.colorScheme.primary,
+              ),
+              recognizer: TapGestureRecognizer()..onTap = onSignup,
+              //?Chain method works somehow...
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SignupForm extends StatefulWidget {
+  const SignupForm({super.key});
+
+  @override
+  State<SignupForm> createState() => _SignupFormState();
+}
+
+class _SignupFormState extends State<SignupForm> {
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+  final TextEditingController _passwordRetype = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final local = AppLocalizations.of(context);
+    final theme = Theme.of(context);
+
+    void onSignupAttempt() {}
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const SizedBox(
+          height: 50,
+        ),
+        Text(
+          local.sign_up_title,
+          style: theme.textTheme.headlineMedium,
+        ),
+        const SizedBox(
+          height: 50,
+        ),
+        SignupEmailField(emailController: _email),
+        const SizedBox(
+          height: 20,
+        ),
+        SignupPasswordField(
+          title: local.password,
+          passwordController: _password,
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        SignupPasswordField(
+          title: local.sign_up_retype_password,
+          passwordController: _passwordRetype,
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        LargeButton(
+          text: local.sign_up,
+          action: onSignupAttempt,
+        ),
+      ],
+    );
+  }
+}
+
+class SignupEmailField extends StatelessWidget {
+  const SignupEmailField({
+    super.key,
+    required TextEditingController emailController,
+  }) : _email = emailController;
+
+  final TextEditingController _email;
+
+  @override
+  Widget build(BuildContext context) {
+    final local = AppLocalizations.of(context);
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 10.0),
+          child: Text(local.email_address),
+        ),
+        const SizedBox(
+          height: 10.0,
+        ),
+        TextField(
+          controller: _email,
+          keyboardType: TextInputType.emailAddress,
+          autocorrect: false,
+          enableSuggestions: false,
+          decoration: InputDecoration(
+            filled: true,
+            hintText: local.email_example,
+            border: OutlineInputBorder(
+              borderSide: BorderSide.none,
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class SignupPasswordField extends StatefulWidget {
+  const SignupPasswordField({
+    super.key,
+    required TextEditingController passwordController,
+    required this.title,
+  }) : _password = passwordController;
+
+  final TextEditingController _password;
+  final String title;
+
+  @override
+  State<SignupPasswordField> createState() => _SignupPasswordFieldState();
+}
+
+class _SignupPasswordFieldState extends State<SignupPasswordField> {
+  bool passwordVisibility = false;
+
+  @override
+  Widget build(BuildContext context) {
+    void onVisibilityChanged() {
+      setState(() {
+        passwordVisibility = !passwordVisibility;
+      });
+    }
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 10.0),
+          child: Text(widget.title),
+        ),
+        const SizedBox(
+          height: 10.0,
+        ),
+        TextField(
+          controller: widget._password,
+          keyboardType: TextInputType.visiblePassword,
+          autocorrect: false,
+          enableSuggestions: false,
+          obscureText: !passwordVisibility,
+          decoration: InputDecoration(
+            filled: true,
+            border: OutlineInputBorder(
+              borderSide: BorderSide.none,
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            suffixIcon: IconButton(
+              onPressed: onVisibilityChanged,
+              icon: Icon(
+                (passwordVisibility)
+                    ? Icons.visibility //
+                    : Icons.visibility_off,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
