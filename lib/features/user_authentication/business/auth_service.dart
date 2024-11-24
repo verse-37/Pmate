83638b07cb185e9981baff87cf7ab22fb4b3d343
verse-37 <verse37theone@gmail.com>
2024-11-24 21:43:51 +0700

    v0.1.19. Auth sign-up implemented. toastification dependency imported

--- lib/features/user_authentication/business/auth_service.dart
+++ lib/features/user_authentication/business/auth_service.dart
@@ -1,11 +1,18 @@
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:pmate/env/common/snackbars.dart';
 
 class AuthService {
   Future<void> signUp({
     required String email,
     required String password,
+    required BuildContext context,
   }) async {
+    final local = AppLocalizations.of(context);
+
     try {
       await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: email,
@@ -13,7 +20,26 @@ class AuthService {
       );
     } on FirebaseAuthException catch (e) {
       Logger().e(e.code);
-      //!should
+      String message = '';
+      switch (e.code) {
+        case 'weak-password':
+          message = local.auth_password_too_weak;
+          break;
+        case 'email-already-in-use':
+          message = local.auth_email_already_exists;
+          break;
+        default:
+          message = local.auth_unidentified_error;
+          break;
+      }
+      if (context.mounted) {
+        //If the parent widget still exists...
+        SnackbarGenerator(
+          title: local.auth_failed,
+          message: message,
+          contentType: ContentType.failure,
+        ).showSnackbar(context);
+      }
     } catch (e) {
       Logger().e(e);
     }
