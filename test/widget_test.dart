efd3dcd46be0570ef6a06eba1b2b4c37e80cbd79
verse-37 <verse37theone@gmail.com>
2024-11-26 12:37:11 +0700

    v0.2.0. flutter_spinkit dependency is imported. Everything is ready for the next phase

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:pmate/features/navigation/app.dart';
+import 'package:pmate/features/app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
